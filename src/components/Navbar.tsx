c8f6ea30c08f9ba1af351983cc45efed6d8b2d72
Arman Mia <mdarman365506@gmail.com>
2025-10-10 11:35:53 +0600

    added active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,88 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 const Navbar = () => {
+  const pathname = usePathname()
+  console.log(pathname);
     return (
       <nav className="w-full bg-gray-700 text-white px-6 py-3 flex items-center justify-between">
         <div className="text-xl font-bold">
-          <Link href='/' >Next Js</Link>
+          <Link href="/">Next Js</Link>
         </div>
         <div className="flex space-x-6">
-          <Link href="/about" className="hover:text-gray-300">
+          <Link
+            href="/about"
+            className={`
+            ${
+              pathname === "/about"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             About
           </Link>
-          <Link href="/gallery" className="hover:text-gray-300">
+          <Link
+            href="/gallery"
+            className={`
+            ${
+              pathname === "/gallery"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             Gallery
           </Link>
-          <Link href="/products" className="hover:text-gray-300">
+          <Link
+            href="/products"
+            className={`
+            ${
+              pathname === "/products"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             products
           </Link>
-          <Link href="/contact" className="hover:text-gray-300">
+          <Link
+            href="/contact"
+            className={`
+            ${
+              pathname === "/contact"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             Contact
           </Link>
-          <Link href="/dashboard" className="hover:text-gray-300">
+          <Link
+            href="/dashboard"
+            className={`
+            ${
+              pathname === "/dashboard"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             Dashboard
           </Link>
-          <Link href="/login" className="hover:text-gray-300">
+          <Link
+            href="/login"
+            className={`
+            ${
+              pathname === "/login"
+                ? "text-blue-700 font-semibold underline"
+                : "text-white hover:underline"
+            }
+            `}
+          >
             Login
           </Link>
         </div>
